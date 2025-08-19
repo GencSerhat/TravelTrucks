8ce0085a6bae367a94931806742f1b9b2485d428
Serhat GENÇ <gncsrht05@gmail.com>
2025-08-19 22:59:43 +0300

    TravelTrucks home bitti

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import Navbar from "../Navbar/Navbar";
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <section className={styles.HomeBanner}>
+        {/* <div>
+          <img
+            src="../../../public/HomeBanner.png"
+            alt="HomeBanner"
+            className={styles.HomeBannerImage}
+          />
+        </div> */}
+        <div className={styles.HomeTextContainer}>
+          <h1 className={styles.HomeTextH1}>Campers of your dreams</h1>
+          <h2 className={styles.HomeTextH2}>
+            You can find everything you want in our catalog
+          </h2>
+
+          <button className={styles.ViewButton}>View Now</button>
+        </div>
+      </section>
+    </>
+  );
+}
+export default Home;
