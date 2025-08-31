f86c81438d5e46e2847b368f6c15b5a6819c8b91
Serhat GENÇ <gncsrht05@gmail.com>
2025-08-31 12:35:20 +0300

    logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
       <nav className={styles.Navbar}>
         <div className={styles.logoDiv}>
           <img
-            src={`${import.meta.env.BASE_URL}logo.png`}
+            src={`/logo.png`}
             alt="TravelTrucks"
             className={styles.LogoNavbar}
           />
